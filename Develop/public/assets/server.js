46de9b4fae2e82ad188af572854fabf6309de0af
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-09-16 13:12:25 -0400

    added dependencies

---
+++ Develop/public/assets/server.js
@@ -0,0 +1,10 @@
+const express = require("require");
+const path = require('path');
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json());
